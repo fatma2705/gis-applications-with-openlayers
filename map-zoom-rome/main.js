2af914b598345e41fc41840f0bf18418cfd4a2e6
fatma2705 <arafatma2003@gmail.com>
2024-11-22 16:37:22 +0100

    Search bar aggiunta

--- map-zoom-rome/main.js
+++ map-zoom-rome/main.js
@@ -1,8 +1,11 @@
 import './style.css';
+import 'ol/ol.css';
+import 'ol-ext/dist/ol-ext.css'; 
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import { fromLonLat } from 'ol/proj';
+import SearchNominatim from 'ol-ext/control/SearchNominatim';
 
 const map = new Map({
   target: 'map',
@@ -16,8 +19,19 @@ const map = new Map({
     zoom: 2
   })
 });
- document.getElementById('zoomToRome').addEventListener('click', () => {
+ document.getElementById('zoomRoma').addEventListener('click', () => {
   const romaCoordinate = fromLonLat([12.545,41.8992]);
   map.getView().setCenter(romaCoordinate);
   map.getView().setZoom(10);
  })
+ const search = new SearchNominatim({
+   placeholder: 'Cerca un luogo...',
+  title: 'Ricerca Geocoding',
+  
+});
+map.addControl(search)
+search.on('select', function (e) {
+  const coord = e.coordinate;
+  map.getView().setCenter(coord);
+  map.getView().setZoom(12);
+});
